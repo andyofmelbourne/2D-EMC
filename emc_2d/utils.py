80b4296686ee6a796a03214a385d743cb6cfcdb5
Andrew James Morgan <andyofmelbourne@gmail.com>
2024-03-01 11:09:17 +1100

    add mpi back to utils

--- emc_2d/utils.py
+++ emc_2d/utils.py
@@ -8,11 +8,10 @@ import runpy
 import h5py
 
 
-#from mpi4py import MPI
-#comm = MPI.COMM_WORLD
-#rank = comm.Get_rank()
-#size = comm.Get_size()
-rank = 0
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
 
 if rank == 0 :
     silent = False
